de708ee9d2bff8771243bc5e3ff637b6be2b54f6
Junhyoung <junh3951@me.com>
2024-07-11 10:54:27 +0900

    init

---
+++ FE/src/app/code_message.js
@@ -0,0 +1,28 @@
+const CodeMessage = [
+  {
+    code: "DATA_UPDATE_SUCCESS",
+    message: "데이터베이스를 업데이트하는데 성공했습니다.",
+  },
+  {
+    code: "DATA_UPDATE_FAIL",
+    message: "데이터베이스를 업데이트하는데 실패했습니다.",
+  },
+  {
+    code: "DATA_READ_SUCCESS",
+    message: "데이터베이스를 불러오는데 성공했습니다.",
+  },
+  {
+    code: "DATA_READ_FAIL",
+    message: "데이터베이스를 불러오는데 실패했습니다.",
+  },
+  {
+    code: "DOMAIN_QUOTE_UPLOAD_SUCCESS",
+    message: "Question을 데이터베이스에 업로드하는데 성공했습니다.",
+  },
+  {
+    code: "DOMAIN_QUOTE_UPLOAD_FAIL",
+    message: "Question을 데이터베이스에 업로드하는데 실패했습니다.",
+  },
+];
+
+export default CodeMessage;
---
+++ FE/src/app/sampledata.js
@@ -0,0 +1,53 @@
+const sampleYoutuber = [
+  {
+    hashtag: "범죄와사회",
+    createdAt: new Date(),
+    question:
+      "Q. 어떤 원칙에 따르면, 가장 강력한 국가부터 가장 작은 국가까지, 모든 국가는 ______, 자기 결정권, 그리고...",
+    answer: "주권적 평등성",
+  },
+  {
+    hashtag: "범죄와사회",
+    createdAt: new Date(),
+    question:
+      "Q. 어떤 원칙에 따르면, 가장 강력한 국가부터 가장 작은 국가까지, 모든 국가는 ______, 자기 결정권, 그리고...",
+    answer: "주권적 평등성",
+  },
+  {
+    hashtag: "범죄와사회",
+    createdAt: new Date(),
+    question:
+      "Q. 어떤 원칙에 따르면, 가장 강력한 국가부터 가장 작은 국가까지, 모든 국가는 ______, 자기 결정권, 그리고...",
+    answer: "주권적 평등성",
+  },
+  {
+    hashtag: "범죄와사회",
+    createdAt: new Date(),
+    question:
+      "Q. 어떤 원칙에 따르면, 가장 강력한 국가부터 가장 작은 국가까지, 모든 국가는 ______, 자기 결정권, 그리고...",
+    answer: "주권적 평등성",
+  },
+  {
+    hashtag: "범죄와사회",
+    createdAt: new Date(),
+    question:
+      "Q. 어떤 원칙에 따르면, 가장 강력한 국가부터 가장 작은 국가까지, 모든 국가는 ______, 자기 결정권, 그리고...",
+    answer: "주권적 평등성",
+  },
+  {
+    hashtag: "범죄와사회",
+    createdAt: new Date(),
+    question:
+      "Q. 어떤 원칙에 따르면, 가장 강력한 국가부터 가장 작은 국가까지, 모든 국가는 ______, 자기 결정권, 그리고...",
+    answer: "주권적 평등성",
+  },
+  {
+    hashtag: "범죄와사회",
+    createdAt: new Date(),
+    question:
+      "Q. 어떤 원칙에 따르면, 가장 강력한 국가부터 가장 작은 국가까지, 모든 국가는 ______, 자기 결정권, 그리고...",
+    answer: "주권적 평등성",
+  },
+];
+
+export { sampleYoutuber };
